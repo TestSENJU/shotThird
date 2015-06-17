1cc2eeb72bd7908d4ffbec33299e0161bf84e3a1
njuser <865717138@qq.com>
2015-06-17 13:39:20 +0800

    逻辑过一会交一点之一

--- IterationThird/src/main/java/logic/MatchBL_Impl.java
+++ IterationThird/src/main/java/logic/MatchBL_Impl.java
@@ -1,58 +1,85 @@
 package logic;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Set;
 
+import Data.MatchData;
+import Data.MatchData_Impl;
 import VO.MatchShortVO;
 import VO.MatchVO;
 import VO.PlayerShortVO;
+
 /**
  * at the moment this class is still a stub
+ * 
  * @author Administrator
  *
  */
-public class MatchBL_Impl implements MatchBL{
+public class MatchBL_Impl implements MatchBL {
 
-	  private static MatchBL_Impl instance ;
-	    private MatchBL_Impl() {
+	MatchData md = MatchData_Impl.getInstance();
+	Hashtable<String, MatchVO> mhb = new Hashtable<String, MatchVO>();
+	Hashtable<String, MatchShortVO> mshb = new Hashtable<String, MatchShortVO>();
+	private static MatchBL_Impl instance;
 
-	    }
-	    public static synchronized MatchBL_Impl getInstance() {  
-	    	if (instance == null) {  
-	    		 instance = new MatchBL_Impl();  
-	    	 }  
-	    	 return instance;  
-	    	 }
-		@Override
-		public MatchVO getMatchByID(String matchid) {
-			// TODO Auto-generated method stub
-			return null;
+	private MatchBL_Impl() {
+		mhb = md.getMatchvotable();
+		mshb = md.getMatchShorttable();
+	}
+
+	public static synchronized MatchBL_Impl getInstance() {
+		if (instance == null) {
+			instance = new MatchBL_Impl();
 		}
-		@Override
-		public ArrayList<MatchShortVO> getAllMatches(int num) {
-			// TODO Auto-generated method stub
-			return null;
+		return instance;
+	}
+
+	@Override
+	public MatchVO getMatchByID(String matchid) {
+		return mhb.get(matchid);
+	}
+
+	@Override
+	public ArrayList<MatchShortVO> getAllMatches(int num) {
+		ArrayList<MatchShortVO> l = new ArrayList<MatchShortVO>();
+		Set<String> keys = mshb.keySet();
+		for (String key : keys) {
+			l.add(mshb.get(key));
 		}
-		@Override
-		public ArrayList<MatchShortVO> getRecentMatches(int num) {
-			// TODO Auto-generated method stub
-			return null;
+		return l;
+	}
+
+	@Override
+	public ArrayList<MatchShortVO> getRecentMatches(int num) {
+		// TODO
+		ArrayList<MatchShortVO> l = new ArrayList<MatchShortVO>();
+		Set<String> keys = mshb.keySet();
+		for (String key : keys) {
+			l.add(mshb.get(key));
 		}
-		@Override
-		public ArrayList<String> getScoreListByShortVO(MatchShortVO match) {
-			// TODO Auto-generated method stub
-//			ArrayList<String>list=data.getMatchDetail(match.getMatchid()).getScorelist();
-			return null;
-		}
-		@Override
-		public ArrayList<PlayerShortVO> getMatchKing(String matchid, int option) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public ArrayList<MatchShortVO> getMatchesByConditions(String teamname,
-				String playername, String time, String season, int after) {
-			// TODO Auto-generated method stub
-			return null;
-		}
+		return l;
+	}
+
+	@Override
+	public ArrayList<String> getScoreListByShortVO(MatchShortVO match) {
+		// TODO Auto-generated method stub
+		// ArrayList<String>list=data.getMatchDetail(match.getMatchid()).getScorelist();
+		return null;
+	}
+
+	// @Override
+	// public ArrayList<PlayerShortVO> getMatchKing(String matchid, int option)
+	// {
+	// // TODO Auto-generated method stub
+	// return null;
+	// }
+
+	@Override
+	public ArrayList<MatchShortVO> getMatchesByConditions(String teamname,
+			String playername, String time, String season, int after) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- IterationThird/src/main/java/logic/TeamBL_Impl.java
+++ IterationThird/src/main/java/logic/TeamBL_Impl.java
@@ -1,131 +1,156 @@
 package logic;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
+import Data.TeamData;
+import Data.TeamData_Impl;
 import VO.MatchShortVO;
+import VO.MatchVO;
 import VO.PlayerShortVO;
 import VO.TeamHotVO;
 import VO.TeamInfoVO;
 import VO.TeamNumberVO;
 import VO.TeamRateVO;
 
-public class TeamBL_Impl implements TeamBL{
-	  private static TeamBL_Impl instance ;
-	    private TeamBL_Impl() {
+public class TeamBL_Impl implements TeamBL {
+	TeamData td = TeamData_Impl.getInstance();
+	Hashtable<String, TeamInfoVO> ifhb = new Hashtable<String, TeamInfoVO>();
+	private static TeamBL_Impl instance;
 
-	    }
-	    public static synchronized TeamBL_Impl getInstance() {  
-	    	if (instance == null) {  
-	    		 instance = new TeamBL_Impl();  
-	    	 }  
-	    	 return instance;  
-	    	 }
-		@Override
-		public ArrayList<PlayerShortVO> getTeamPlayers(String teamname) {
-			// TODO Auto-generated method stub
-			return null;
+	private TeamBL_Impl() {
+		ifhb = td.getTeamInfo();
+	}
+
+	public static synchronized TeamBL_Impl getInstance() {
+		if (instance == null) {
+			instance = new TeamBL_Impl();
 		}
-		@Override
-		public TeamRateVO getTeamRate(String teamname, String season,
-				int isAfter) {
-			// TODO Auto-generated method stub
-			return new TeamRateVO("NOP") ;
-		}
-		@Override
-		public TeamNumberVO getTeamAverageNumber(String teamname,
-				String season, int isAfter) {
-			// TODO Auto-generated method stub
-			return new TeamNumberVO("NOP") ;
-		}
-		@Override
-		public TeamNumberVO getTeamAllNumber(String teamname, String season,
-				int isAfter) {
-			// TODO Auto-generated method stub
-			return new TeamNumberVO("NOP") ;
-		}
-		@Override
-		public ArrayList<TeamHotVO> getSeasonHotTeam(int option) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public ArrayList<TeamNumberVO> getTeamAverageNumber(String season,
-				int after, int rate) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public ArrayList<TeamNumberVO> getTeamAllNumber(String season,
-				int after, int rate) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public ArrayList<TeamRateVO> getTeamRate(String season, int after,
-				int rate) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public TeamHotVO getSeasonTeamAverage(String teamname, String season,
-				int after, int option) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public TeamHotVO getSeasonAverage(String season, int after, int option) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public TeamHotVO getSeasonTeamMedian(String teamname, String season,
-				int after, int option) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public TeamHotVO getSeasonMedian(String season, int after, int option) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public TeamHotVO getSeasonTeamVariance(String teamname, String season,
-				int after, int option) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public TeamHotVO getSeasonVariance(String season, int after, int option) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public ArrayList<MatchShortVO> getMatchesByTeam(String teamname,
-				String season, int after) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public ArrayList<TeamHotVO> getSeasonTeamA(String team, String season,
-				int after) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public ArrayList<TeamHotVO> getSeasonTeamV(String team, String season,
-				int after) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public ArrayList<TeamHotVO> getSeasonTeamM(String team, String season,
-				int after) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public TeamInfoVO getTeamInfo(String teamname) {
-			// TODO Auto-generated method stub
-			return null;
-		}
+		return instance;
+	}
+
+	@Override
+	public ArrayList<PlayerShortVO> getTeamPlayers(String teamname) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeamRateVO getTeamRate(String teamname, String season, int isAfter) {
+		// TODO Auto-generated method stub
+		return new TeamRateVO("NOP");
+	}
+
+	@Override
+	public TeamNumberVO getTeamAverageNumber(String teamname, String season,
+			int isAfter) {
+		// TODO Auto-generated method stub
+		return new TeamNumberVO("NOP");
+	}
+
+	@Override
+	public TeamNumberVO getTeamAllNumber(String teamname, String season,
+			int isAfter) {
+		// TODO Auto-generated method stub
+		return new TeamNumberVO("NOP");
+	}
+
+	@Override
+	public ArrayList<TeamHotVO> getSeasonHotTeam(int option) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<TeamNumberVO> getTeamAverageNumber(String season,
+			int after, int rate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<TeamNumberVO> getTeamAllNumber(String season, int after,
+			int rate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<TeamRateVO> getTeamRate(String season, int after, int rate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeamHotVO getSeasonTeamAverage(String teamname, String season,
+			int after, int option) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeamHotVO getSeasonAverage(String season, int after, int option) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeamHotVO getSeasonTeamMedian(String teamname, String season,
+			int after, int option) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeamHotVO getSeasonMedian(String season, int after, int option) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeamHotVO getSeasonTeamVariance(String teamname, String season,
+			int after, int option) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeamHotVO getSeasonVariance(String season, int after, int option) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<MatchShortVO> getMatchesByTeam(String teamname,
+			String season, int after) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<TeamHotVO> getSeasonTeamA(String team, String season,
+			int after) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<TeamHotVO> getSeasonTeamV(String team, String season,
+			int after) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<TeamHotVO> getSeasonTeamM(String team, String season,
+			int after) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeamInfoVO getTeamInfo(String teamname) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
